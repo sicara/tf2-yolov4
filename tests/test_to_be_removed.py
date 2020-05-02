114076c0ffebef47504b9e170c29c44eb129b4ce
AntoineT <antoinet@sicara.com>
2020-05-02 09:27:13 +0200

    Add tautology test to prevent pytest from failing

---
+++ tests/test_to_be_removed.py
@@ -0,0 +1,8 @@
+"""
+This file is to prevent pytest from failing as there is no test.
+Please remove this file as you commit the first real test.
+"""
+
+
+def test_to_be_removed():
+    assert True
