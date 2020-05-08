3d9446f1d375b39a636a74e0075e93ea88c2a8cf
Raphael Meudec <raphaelm@sicara.com>
2020-05-08 17:29:04 +0200

    Add first test on the repo

--- tests/test_to_be_removed.py
+++
@@ -1,9 +0,0 @@
-"""
-This file is to prevent pytest from failing as there is no test.
-Please remove this file as you commit the first real test.
-"""
-import tf2_yolov4
-
-
-def test_to_be_removed():
-    assert True
