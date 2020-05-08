3c1aa1fc874660150cd80c004f186a63af2b4b39
Raphael Meudec <raphaelm@sicara.com>
2020-05-08 19:30:31 +0200

    Add YOLOv4 model

---
+++ tf2_yolov4/model.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+from tf2_yolov4.backbone import CSPDarknet53
+
+
+class YOLOv4(tf.keras.Model):
+    def __init__(self, input_shape):
+        super(YOLOv4, self).__init__()
+        self.cspdarknet53 = CSPDarknet53(input_shape)
+
+    def call(self, inputs, training=None, mask=None):
+        return self.cspdarknet53(inputs)
+
+
+
+if __name__ == "__main__":
+    model = YOLOv4((416, 416, 3))
+    outputs = model.predict(tf.random.uniform((16, 416, 416, 3)))
+    print(outputs[0].shape)
