f4023f70099461e7cd1165738177eda50ce44ddb
Raphael Meudec <raphaelm@sicara.com>
2020-05-09 00:32:26 +0200

    Black

--- tf2_yolov4/model.py
+++ tf2_yolov4/model.py
@@ -1,16 +1,25 @@
+"""
+Model class for YOLOv4
+"""
 import tensorflow as tf
 
 from tf2_yolov4.backbone import CSPDarknet53
 
 
 class YOLOv4(tf.keras.Model):
+    """YOLOv4 Model"""
     def __init__(self, input_shape):
+        """
+        Constructor
+
+        Args:
+            input_shape (Tuple[int]): Input shape of the image
+        """
         super(YOLOv4, self).__init__()
         self.cspdarknet53 = CSPDarknet53(input_shape)
 
     def call(self, inputs, training=None, mask=None):
         return self.cspdarknet53(inputs)
-
 
 
 if __name__ == "__main__":
