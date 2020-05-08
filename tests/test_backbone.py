89a978e83bc86f08deea1f7e1a002acee0afdc41
Raphael Meudec <raphaelm@sicara.com>
2020-05-08 17:29:21 +0200

    Black pass

--- tests/test_backbone.py
+++ tests/test_backbone.py
@@ -3,4 +3,3 @@ def test_backbone_should_have_valid_output_shapes(cspdarknet53_416):
     assert output_1.shape.as_list() == [None, 52, 52, 256]
     assert output_2.shape.as_list() == [None, 26, 26, 512]
     assert output_3.shape.as_list() == [None, 13, 13, 1024]
-
