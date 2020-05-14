9766b58d97da90ac0298a5219ca019369f66c668
AntoineT <antoinet@sicara.com>
2020-05-14 22:32:29 +0200

    Add test to check the csp_darknet53 number of params

--- tests/test_csp_darknet53.py
+++ tests/test_csp_darknet53.py
@@ -3,3 +3,7 @@ def test_backbone_should_have_valid_output_shapes(cspdarknet53_416):
     assert output_1.shape.as_list() == [None, 13, 13, 1024]
     assert output_2.shape.as_list() == [None, 26, 26, 512]
     assert output_3.shape.as_list() == [None, 52, 52, 256]
+
+
+def test_backbone_should_have_the_right_number_of_parameters(cspdarknet53_416):
+    assert 26652512 == cspdarknet53_416.count_params()
